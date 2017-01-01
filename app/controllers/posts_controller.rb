65166901e4c04ecfb0f9c53864846942a069d696
yuansq998 <yuansq998@163.com>
2017-01-01 10:32:01 +0800

    use simple_format helper to decorate

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -7,18 +7,49 @@ class PostsController < ApplicationController
     @post = Post.new
   end
 
+  def show
+    @group = Group.find(params[:group_id])
+    @post = Post.find(params[:id])
+  end
+  def edit
+    @group = Group.find(params[:group_id])
+    @post = Post.find(params[:id])
+  end
+  def index
+    @groups = Group.all
+    @posts = Post.all
+  end
+
+
+
   def create
     @group = Group.find(params[:group_id])
     @post = Post.new(post_params)
     @post.group = @group
     @post.user = current_user
-
     if @post.save
       redirect_to group_path(@group)
     else
       render :new
     end
   end
+
+  def update
+    @group = Group.find(params[:group_id])
+    @post.group = @group
+    @post.user = current_user
+    if @post.update(post_params)
+      redirect_to group_path(@group)
+    else
+      render :edit
+    end
+  end
+  def  destroy
+    @group = Group.find(params[:group_id])
+    Post.Delete
+    redirect_to group_path(@group)
+  end
+
 
 
   private
